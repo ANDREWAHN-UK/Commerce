d9d4a80319f446f4eba809d9d9b4c5de5664173a
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-12-23 18:41:31 +0000

    store/admin.py - add and adjust class ProductAdmin so it doesn't throw up errors in django admin panel

--- store/admin.py
+++ store/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Product, Customer
 # Register your models here.
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',        
+        'price',
+        'image',
+    )
+
+
 admin.site.register(Customer)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
