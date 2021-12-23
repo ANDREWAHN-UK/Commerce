e63f8889ab813aa71c084257c21c3b86bb70d2cd
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-12-23 18:40:30 +0000

    bestburger/urls.py - tidy up code

--- bestburger/urls.py
+++ bestburger/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
 
@@ -29,5 +29,5 @@ urlpatterns = [
 ]
 
 
-#the below tells the project that images can be accessed with a url
+#  the below tells the project that images can be accessed with a url
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
