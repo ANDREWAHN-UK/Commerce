22bd8d64a6e4182dad47c8880e0237e3c9299e44
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-12-23 17:30:31 +0000

    update bestburger urls and settings; update cart.html from cart/templates/cart.html to cart/templates/cart/cart.html; repeat for checkout, sotore  and home.html; upload images for use in navbar and as placeholders in store.html, create script.js so the static/scripts folder is visible; update main.css with styling

--- bestburger/settings.py
+++ bestburger/settings.py
@@ -64,7 +64,8 @@ ROOT_URLCONF = 'bestburger.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- cart/views.py
+++ cart/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 
 def cart(request):
     context = {}
-    return render(request, 'checkout/checkout.html', context)
+    return render(request, 'cart/cart.html', context)
