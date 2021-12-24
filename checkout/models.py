6af9ac22fe5ab0a158f0158a5fd1ba8ceae2ff22
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-12-24 12:02:39 +0000

    checkout - update views.py to have correct data in context; update models to import correct models; update checkout.html to reflect correct data and to navigate better

--- checkout/models.py
+++ checkout/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from store.models import Product, Customer
+from store.models import *
 from django.conf import settings
 
 # Create your models here.
--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from .models import Order, OrderItem, ShippingAddress
-
+from store.models import *
+from .models import *
 # Create your views here.
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        # Create empty cart for now for non-logged in user
+        # this is because the template will loop through the whole list, so there needs to be something here
+        items = []	
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {'items':items, 'order':order}
     return render(request, 'checkout/checkout.html', context)
