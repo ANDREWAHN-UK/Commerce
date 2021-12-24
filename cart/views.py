684eb28e190903dd0caf8a6150a75a26fa2e2656
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-12-24 11:32:50 +0000

    cart - update def cart context to have order; update cart.html so templates show values retrieved

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-
+from store.models import *
+from checkout.models import *
 # Create your views here.
 
 
+# see documentation on get or create https://docs.djangoproject.com/en/4.0/ref/models/querysets/#get-or-create
+
 def cart(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        # Create empty cart for now for non-logged in user
+        # this is because the template will loop through the whole list, so there needs to be something here
+        items = []	
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {'items':items, 'order':order}
     return render(request, 'cart/cart.html', context)
