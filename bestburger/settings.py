809bc4341b4adc9504fd82564044f24981b37507
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-12-27 10:15:26 +0000

    bestburger/settings.py - update setting with stripe public and private key linked to os.env

--- bestburger/settings.py
+++ bestburger/settings.py
@@ -166,3 +166,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
