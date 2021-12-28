4cff1ae3aa183d0bc0dfa327783faee2cb04c7bb
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-12-28 15:54:00 +0000

    checkout -update views; update urls; update html with csrf token

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-
-
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('update_item/', views.updateItem, name="update_item"),
--- checkout/views.py
+++ checkout/views.py
@@ -3,6 +3,7 @@ from checkout.models import Order
 from .models import *
 from store.views import updateItem
 import stripe
+from bestburger import settings
 
 # Create your views here.
 
@@ -14,28 +15,33 @@ def checkout(request):
         cartItems = order.get_cart_items
     else:
         # Create empty cart for now for non-logged in user
-        # this is because the template will loop through the whole list, so there needs to be something here
+        # this is because the template will loop through the whole list,
+        #  so there needs to be something here
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
+        cartItems = order['get_cart_items']
 
     stripe.api_key = 'sk_test_51K8jexEE3VLVHzWcHSDIRoXnm3cGSze1zo4WDrHeSMwLQjO269ds452ALbYGlliIeTcdqzW7qEc82cOtrKUILdq100uvmXF6cq'
 
-    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    # public key needs to be displayed here so stripe_elements can access it
+    stripe_public_key = 'pk_test_51K8jexEE3VLVHzWc8ckIVJpmSWAhlhOnT7nz8ioOry3z0atx9lyoXk3K2njUw23ZsTs21nofTig1QnoY31ni4H0s00njCJPUBc'
     stripe_secret_key = settings.STRIPE_SECRET_KEY
+    # the'amount' variable  is also displayed in the cart and checkout apps
+    # it needs to be a number like 1234 instead of 12.34, for stripe reasons
+    amount = round(order.get_cart_total*100)
 
     intent = stripe.PaymentIntent.create(
-        customer='{{customer}}',
-        currency="usd",
-        amount=2000,
+        amount=amount,
+        currency=settings.STRIPE_CURRENCY,
         payment_method_types=["card"],
         setup_future_usage="on_session",
         )
 
     context = {
-        'items': items, 
-        'order': order, 
+        'items': items,
+        'order': order,
         'cartItems': cartItems,
-        'stripe_public_key':stripe_public_key,
+        'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
          }
     return render(request, 'checkout/checkout.html', context)
