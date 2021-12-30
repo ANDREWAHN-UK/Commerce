c065ebce92787ce9a73c1347eae066ab257dbbd9
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-12-30 13:10:48 +0000

    cart/nodels.py - create class CartItem; make and apply migrations

--- cart/models.py
+++ cart/models.py
@@ -1,4 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from store.models import Product
 
 # Create your models here.
+
+class CartItem(models.Model):
+    cart_id = models.CharField(max_length=50)
+    date_added = models.DateField(auto_now_add=True)
+    quantity = models.IntegerField(default=1)
+    product = models.ForeignKey(Product, unique=False, on_delete=models.CASCADE, null=True)
+
+    def total(self):
+        return self.quantity * self.product.price
+
+    def name(self):
+        return self.product.name
+    
+    def price(self):
+        return self.product.price
+
+    def augment_quantity(self, quantity):
+        self.quantity = self.quantity + int(quantity)
+        self.save
