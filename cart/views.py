22dc605ae94ad244367bf2a776488d4a5353dc58
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-12-30 20:01:44 +0000

    status

--- cart/views.py
+++ cart/views.py
@@ -31,61 +31,63 @@ def cart(request):
     return render(request, 'cart/cart.html', context)
 
 
-def view_cart(request):
-    """ A view that renders the cart contents page """
+# below is CI stuff, commented out as didn't play well
+# def view_cart(request):
+#     """ A view that renders the cart contents page """
 
-    return render(request, 'cart/cart.html')
+#     return render(request, 'cart/cart.html')
 
-def add_to_cart(request, item_id):
-    """ Add a quantity of the specified product to the shopping cart """
 
-    product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
+# def add_to_cart(request, item_id):
+#     """ Add a quantity of the specified product to the shopping cart """
+
+#     product = get_object_or_404(Product, pk=item_id)
+#     quantity = int(request.POST.get('quantity'))
+#     redirect_url = request.POST.get('redirect_url')
   
-    cart = request.session.get('cart', {})
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
-        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
-    else:
-        cart[item_id] = quantity
-        messages.success(request, f'Added {product.name} to your cart')
+#     cart = request.session.get('cart', {})
+#     if item_id in list(cart.keys()):
+#         cart[item_id] += quantity
+#         messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
+#     else:
+#         cart[item_id] = quantity
+#         messages.success(request, f'Added {product.name} to your cart')
 
-    request.session['cart'] = cart
-    return redirect(redirect_url)
+#     request.session['cart'] = cart
+#     return redirect(redirect_url)
     
 
-def update_cart(request, item_id):
-    """Adjust the quantity of the specified product to the specified amount"""
+# def update_cart(request, item_id):
+#     """Adjust the quantity of the specified product to the specified amount"""
 
-    product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
-    cart = request.session.get('cart', {})
-    if quantity > 0:
-        cart[item_id] = quantity
-        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
-    else:
-        cart.pop(item_id)
-        messages.success(request, f'Removed {product.name} from your cart')
+#     product = get_object_or_404(Product, pk=item_id)
+#     quantity = int(request.POST.get('quantity'))
+#     cart = request.session.get('cart', {})
+#     if quantity > 0:
+#         cart[item_id] = quantity
+#         messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
+#     else:
+#         cart.pop(item_id)
+#         messages.success(request, f'Removed {product.name} from your cart')
 
-    request.session['cart'] = cart
-    return redirect(reverse('view_cart'))
+#     request.session['cart'] = cart
+#     return redirect(reverse('view_cart'))
 
 
-def remove_from_cart(request, item_id):
-    """Remove the item from the shopping cart"""
+# def remove_from_cart(request, item_id):
+#     """Remove the item from the shopping cart"""
 
-    try:
-        product = get_object_or_404(Product, pk=item_id)
+#     try:
+#         product = get_object_or_404(Product, pk=item_id)
         
-        cart = request.session.get('cart', {})
+#         cart = request.session.get('cart', {})
 
-        cart.pop(item_id)
-        messages.success(request, f'Removed {product.name} from your cart')
+#         cart.pop(item_id)
+#         messages.success(request, f'Removed {product.name} from your cart')
 
-        request.session['cart'] = cart
-        return HttpResponse(status=200)
+#         request.session['cart'] = cart
+#         return HttpResponse(status=200)
 
-    except Exception as e:
-        messages.error(request, f'Error removing item: {e}')
-        return HttpResponse(status=500)
+#     except Exception as e:
+#         messages.error(request, f'Error removing item: {e}')
+#         return HttpResponse(status=500)
--- checkout/views.py
+++ checkout/views.py
@@ -15,15 +15,17 @@ from django.contrib import messages
 
 # Create your views here.
 
+
 def checkout(request):
    # public key needs to be displayed here so stripe_elements can access it
     stripe_public_key = 'pk_test_51K8jexEE3VLVHzWc8ckIVJpmSWAhlhOnT7nz8ioOry3z0atx9lyoXk3K2njUw23ZsTs21nofTig1QnoY31ni4H0s00njCJPUBc'
-    stripe_secret_key = settings.STRIPE_SECRET_KEY 
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     stripe.api_key = 'sk_test_51K8jexEE3VLVHzWcHSDIRoXnm3cGSze1zo4WDrHeSMwLQjO269ds452ALbYGlliIeTcdqzW7qEc82cOtrKUILdq100uvmXF6cq'
 
     if request.user.is_authenticated:
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
         product = Product
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
@@ -33,7 +35,7 @@ def checkout(request):
         #  so there needs to be something here
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
-        cartItems = order['get_cart_items']   
+        cartItems = order['get_cart_items']
 
     if request.method == 'POST':
         cart = request.session.get('cart', {})
@@ -42,14 +44,14 @@ def checkout(request):
             'name': request.POST['name'],
             'email': request.POST['email'],
             'address': request.POST['address'],
-            'street': request.POST['street'],            
+            'street': request.POST['street'],
             'city': request.POST['city'],
             'county': request.POST['county'],
             'postcode': request.POST['postcode'],
             'street_address2': request.POST['street_address2'],
             'number': request.POST['number'],
             }
-        
+
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save(commit=False)
@@ -70,9 +72,7 @@ def checkout(request):
                     )
                     order.delete()
                     return redirect(reverse('view_cart'))
-        
-        
-    
+
     # the'amount' variable  is also displayed in the cart and checkout apps
     # it needs to be a number like 1234 instead of 12.34, for stripe reasons
     amount = round(order.get_cart_total*100)
@@ -94,82 +94,95 @@ def checkout(request):
     return render(request, 'checkout/checkout.html', context)
 
 
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+    else:
+        print('User is not logged in')
+    return JsonResponse('Payment submitted...', safe=False)
+    
+
 # below to process order once may payment button is clicked
 # date time generates order id
-def processOrder(request):
+# def processOrder(request):
     
-    transaction_id = datetime.datetime.now().timestamp()
-    # below used to parse a valid JSON string
-    # and convert it into a Python Dictionary, so the views can read it
-    data = json.loads(request.body)
-    # below is some logic to get info from logged in users
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        total = float(data['form']['total'])
-        order.transaction_id = transaction_id
-    else:
-        print('User is not logged in')
-        # make sure the total received matches the cart total, and if so, save the order
-    if total == order.get_cart_total:
-        order.complete = True
-        order.save()
+#     transaction_id = datetime.datetime.now().timestamp()
+#     # below used to parse a valid JSON string
+#     # and convert it into a Python Dictionary, so the views can read it
+#     data = json.loads(request.body)
+#     # below is some logic to get info from logged in users
+#     if request.user.is_authenticated:
+#         customer = request.user.customer
+#         order, created = Order.objects.get_or_create(customer=customer, complete=False)
+#         total = float(data['form']['total'])
+#         order.transaction_id = transaction_id
+#     else:
+#         print('User is not logged in')
+#         # make sure the total received matches the cart total, and if so, save the order
+#     if total == order.get_cart_total:
+#         order.complete = True
+#         order.save()
     
-    # create an instance of the shipping data
-    if order.shipping == True:
-        ShippingAddress.objects.create(
-            customer=customer,
-            order=order,
-            address=data['shipping']['address'],
-            city=data['shipping']['city'],
-            county=data['shipping']['county'],
-            postcode=data['shipping']['postcode'],
-            )
+#     # create an instance of the shipping data
+#     if order.shipping == True:
+#         ShippingAddress.objects.create(
+#             customer=customer,
+#             order=order,
+#             address=data['shipping']['address'],
+#             city=data['shipping']['city'],
+#             county=data['shipping']['county'],
+#             postcode=data['shipping']['postcode'],
+#             )
 
 
-    return JsonResponse('Payment submitted...', safe=False)
+#     return JsonResponse('Payment submitted...', safe=False)
 
 
-def process(request):
-    transaction_id = datetime.datetime.now().timestamp()
-    data = json.loads(request.body)
+# def process(request):
+#     transaction_id = datetime.datetime.now().timestamp()
+#     data = json.loads(request.body)
      
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        total = float(data['form']['total'])
-        order.transaction_id = transaction_id
-    else:
-        print('User is not logged in')
-        # make sure the total received matches the cart total, and if so,
-        #  create an instance of the order
-    if total == order.get_cart_total:
-        order = create_order(request, transaction_id)
-        results = {'order_number': transaction_id, 'message': ''}
+#     if request.user.is_authenticated:
+#         customer = request.user.customer
+#         order, created = Order.objects.get_or_create(customer=customer, complete=False)
+#         total = float(data['form']['total'])
+#         order.transaction_id = transaction_id
+#     else:
+#         print('User is not logged in')
+#         # make sure the total received matches the cart total, and if so,
+#         #  create an instance of the order
+#     if total == order.get_cart_total:
+#         order = create_order(request, transaction_id)
+#         results = {'order_number': transaction_id, 'message': ''}
      
-    return results
+#     return results
 
 
-def create_order(request, transaction_id):
-    order = Order()
-    transaction_id = datetime.datetime.now().timestamp()
-    order.transaction_id = transaction_id
-    order.user = None
-    order.status = order.SUBMITTED
-    order.save()
-    if order.pk:
-        cart_items = OrderItem
-        for ci in cart_items:
-            oi = OrderItem()
-            oi.order = order
-            oi.quantity = ci.quantity
-            oi.price = ci.price
-            oi.product = ci.product
-            oi.save()
-        cart.empty_cart(request)
+# def create_order(request, transaction_id):
+#     order = Order()
+#     transaction_id = datetime.datetime.now().timestamp()
+#     order.transaction_id = transaction_id
+#     order.user = None
+#     order.status = order.SUBMITTED
+#     order.save()
+#     if order.pk:
+#         cart_items = OrderItem
+#         for ci in cart_items:
+#             oi = OrderItem()
+#             oi.order = order
+#             oi.quantity = ci.quantity
+#             oi.price = ci.price
+#             oi.product = ci.product
+#             oi.save()
+#         cart.empty_cart(request)
     
-    return order
+#     return order
 
-def empty_cart(request):
-    user_cart = get_cart_items(request)
-    user_cart.delete()
+# def empty_cart(request):
+#     user_cart = get_cart_items(request)
+#     user_cart.delete()
